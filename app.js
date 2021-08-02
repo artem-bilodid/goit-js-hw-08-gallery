e22d76343f7226c23f38be74a4360031baaed1a1
Artem Bilodid <ar.bilodid@draftkings.com>
2021-08-02 11:55:14 +0300

    added modaj events handling

--- app.js
+++ app.js
@@ -63,3 +63,136 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  lightbox: document.querySelector('.js-lightbox'),
+  lightboxImage: document.querySelector('.js-lightbox .lightbox__image'),
+  lightboxOverlay: document.querySelector('.js-lightbox .lightbox__overlay'),
+  lightboxButton: document.querySelector(
+    'button[data-action="close-lightbox"]'
+  ),
+};
+
+const makeGalleryItemEl = galleryItem => {
+  const galleryItemEl = document.createElement('li');
+  galleryItemEl.classList.add('gallery__item');
+
+  const galleryLinkEl = document.createElement('a');
+  galleryLinkEl.classList.add('gallery__link');
+  galleryLinkEl.href = '#';
+
+  const galleryImageEl = document.createElement('img');
+  galleryImageEl.classList.add('gallery__image');
+  galleryImageEl.src = galleryItem.preview;
+  galleryImageEl.alt = galleryItem.description;
+  galleryImageEl.dataset.original = galleryItem.original;
+
+  galleryLinkEl.appendChild(galleryImageEl);
+  galleryItemEl.appendChild(galleryLinkEl);
+
+  return galleryItemEl;
+};
+
+const galleryItemElList = galleryItems.map(makeGalleryItemEl);
+
+refs.gallery.append(...galleryItemElList);
+
+const addModalEventListeners = () => {
+  refs.lightboxOverlay.addEventListener('click', onLightboxOverlayClick);
+  window.addEventListener('keydown', onEscKeyPressed);
+  window.addEventListener('keydown', onRightButtonPressed);
+  window.addEventListener('keydown', onLeftButtonPressed);
+};
+
+const onGalleryImageClick = event => {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  refs.lightbox.classList.add('is-open');
+  addModalEventListeners();
+
+  refs.lightboxImage.src = event.target.dataset.original;
+  refs.lightboxImage.alt = event.target.alt;
+};
+
+const onGalleryLinkClick = event => {
+  if (!event.target.classList.contains('gallery__link')) {
+    return;
+  }
+
+  refs.lightbox.classList.add('is-open');
+  const childImage = event.target.querySelector('.gallery__image');
+
+  refs.lightboxImage.src = childImage.dataset.original;
+  refs.lightboxImage.alt = childImage.alt;
+
+  addModalEventListeners();
+};
+
+const closeModalAndRemoveModalEventListeners = () => {
+  refs.lightbox.classList.remove('is-open');
+  window.removeEventListener('keydown', onEscKeyPressed);
+  window.removeEventListener('keydown', onRightButtonPressed);
+  window.removeEventListener('keydown', onLeftButtonPressed);
+};
+
+const onLightboxCloseButtonClick = event => {
+  closeModalAndRemoveModalEventListeners();
+};
+
+const onLightboxOverlayClick = event => {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+  closeModalAndRemoveModalEventListeners();
+};
+
+const onEscKeyPressed = event => {
+  if (event.code !== 'Escape') {
+    return;
+  }
+  closeModalAndRemoveModalEventListeners();
+};
+
+const onRightButtonPressed = event => {
+  if (event.code !== 'ArrowRight') {
+    return;
+  }
+
+  const currentItemIndex = galleryItems.findIndex(
+    item => item.original === refs.lightboxImage.src
+  );
+
+  const nextGalleryItem = galleryItems[currentItemIndex + 1];
+
+  if (nextGalleryItem) {
+    refs.lightboxImage.src = nextGalleryItem.original;
+    refs.lightboxImage.alt = nextGalleryItem.alt;
+  }
+};
+
+const onLeftButtonPressed = event => {
+  if (event.code !== 'ArrowLeft') {
+    return;
+  }
+
+  const currentItemIndex = galleryItems.findIndex(
+    item => item.original === refs.lightboxImage.src
+  );
+
+  const prevItemIndex = currentItemIndex - 1;
+
+  const prevGalleryItem = galleryItems[prevItemIndex];
+
+  if (prevGalleryItem) {
+    refs.lightboxImage.src = prevGalleryItem.original;
+    refs.lightboxImage.alt = prevGalleryItem.alt;
+  }
+};
+
+refs.gallery.addEventListener('click', onGalleryImageClick);
+refs.gallery.addEventListener('click', onGalleryLinkClick);
+
+refs.lightboxButton.addEventListener('click', onLightboxCloseButtonClick);
